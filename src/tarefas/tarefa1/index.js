de3fd154423dec3aaebb2a83646c73cf3fe15be4
Hortência Côrtes <hortencia.cortes@gmail.com>
2022-01-25 21:02:43 -0400

    decimal to roman

--- src/tarefas/tarefa1/index.js
+++ src/tarefas/tarefa1/index.js
@@ -1,6 +1,94 @@
+import { useState } from 'react';
 import './styles.css';
 
 const Tarefa1 = () => {
+    const [decimal, setDecimal] = useState('');
+    const [valueRoman, setRoman] = useState('');
+    const objNumOne = {
+        1: [1, "I"],
+        2: [2, "II"],
+        3: [3, "III"],
+        4: [4, "IV"],
+        5: [5, "V"],
+        6: [6, "VI"],
+        7: [7, "VII"],
+        8: [8, "VIII"],
+        9: [9, "IX"],
+    };
+    const objNumTen = {
+        1: [10, "X"],
+        2: [20, "XX"],
+        3: [30, "XXX"],
+        4: [40, "XL"],
+        5: [50, "L"],
+        6: [60, "LX"],
+        7: [70, "LXX"],
+        8: [80, "LXXX"],
+        9: [90, "XC"],
+    };
+    const objNumHundred = {
+        1: [100, "C"],
+        2: [200, "CC"],
+        3: [300, "CCC"],
+        4: [400, "CD"],
+        5: [500, "D"],
+        6: [600, "DC"],
+        7: [700, "DCC"],
+        8: [800, "DCCC"],
+        9: [900, "CM"],
+    };
+    const objNumThousand = {
+        1: [100, "M"],
+        2: [200, "MM"],
+        3: [300, "MMM"],
+    };
+
+    const transformDecimalToRoman = (array, index) => {
+        for (let i = 1; i <= 9; i++) {
+            if (index == i) {
+                return (array[i][1]);
+            }
+        }
+
+        if(index == 0){
+            return ''
+        }
+    }
+
+    const handleDecimalToRoman = () => {
+        if (decimal > 0 && decimal <= 3999) {
+            const newValue = decimal.split("");
+            let resul = '';
+
+            if (newValue.length === 1) {
+                resul = transformDecimalToRoman(objNumOne, newValue[0])
+            }
+
+            if (newValue.length === 2) {
+                resul = transformDecimalToRoman(objNumTen, newValue[0])
+                resul += transformDecimalToRoman(objNumOne, newValue[1])
+            }
+
+            if (newValue.length === 3) {
+                resul = transformDecimalToRoman(objNumHundred, newValue[0])
+                resul += transformDecimalToRoman(objNumTen, newValue[1])
+                resul += transformDecimalToRoman(objNumOne, newValue[2])
+            }
+
+            if (newValue.length === 4) {
+                resul = transformDecimalToRoman(objNumThousand, newValue[0])
+                resul += transformDecimalToRoman(objNumHundred, newValue[1])
+                resul += transformDecimalToRoman(objNumTen, newValue[2])
+                resul += transformDecimalToRoman(objNumOne, newValue[3])
+            }
+            setRoman(resul)
+
+        } else {
+            alert('Digite um valor válido');
+            setRoman('');
+            setDecimal('');
+        }
+    }
 
     return (
         <div className='containerTask'>
@@ -8,20 +96,20 @@ const Tarefa1 = () => {
 
             <div className='lineInput'>
                 <label>
-                    Decimal: <input type="number" />
+                    Decimal: <input type="number" value={decimal} onChange={e => setDecimal(e.target.value)} />
                 </label>
-                <button>Converter para romano</button>
+                <button onClick={handleDecimalToRoman}>Converter para romano</button>
             </div>
 
             <div className='lineInput'>
                 <label>
-                    Romano: <input type="text" />
+                    Romano: <input type="text" value={valueRoman} />
                 </label>
                 <button>Converter para decimal</button>
             </div>
 
         </div>
-    )
+    );
 }
 
 export default Tarefa1;
